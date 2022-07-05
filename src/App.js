322e1934b9d529fe2f5a6471af274357c7429a20
SuperSabre <princewns@gmail.com>
2022-07-05 15:37:23 +0900

    '220705_PWA빌드'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>리액트 웹앱</h1>
+        <p>설치/이동 아이콘을 이용하여 앱 화면을 띄울수있다.</p>
       </header>
     </div>
   );
